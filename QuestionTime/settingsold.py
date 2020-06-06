4802ff64c1e7146269d1bcb14e7826b6eff35ed4
luigigentile <luigi.gentile2@gmail.com>
2020-06-06 18:51:56 +0200

    deploy del 06-06-20

--- QuestionTime/settingsold.py
+++ QuestionTime/settingsold.py
@@ -21,9 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1^c##ad(8kbh%+u-6ww6xs+7lw*dh=89f#hidzp_#r640r1p_4'
-
+import os
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','1^c##ad(8kbh%+u-6ww6xs+7lw*dh=89f#hidzp_#r640r1p_4')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
